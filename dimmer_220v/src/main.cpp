abb5fc4e76a5766f48bfdd0533d82bd193385573
juanfranflores <juan27fp@gmail.com>
2023-11-12 13:37:09 -0300

    Refactorización del PWM y manejo de entrada Serial
    - Reemplazado setValDimmer con setBrillo para reflejar cambios en la configuración de brillo.
    - Modificada la función zeroCrossInt para optimizar la generación de la señal PWM.
    - Mejora en la función fade para transiciones más suaves de brillo.
    - Introducido la variable delaySinceCross para gestionar los retrasos basados en el brillo.
    - Ajustada la función fade para utilizar setBrillo para transiciones de brillo mejoradas. Todavía usa delays.

--- dimmer_220v/src/main.cpp
+++ dimmer_220v/src/main.cpp
@@ -8,12 +8,11 @@
 // Constantes
 const int minBrillo = 0;
 const int maxBrillo = 1000;
-const int minOffset = 100;
-const int maxOffset = 600;
+const int minDelayCross = 4050; // Para el brillo máximo: Disminuir hasta que se vea una diferencia real entre el brillo en 900 y el brillo en 1000
+const int maxDelayCross = 8100; // Para el brillo mínimo: Aumentar hasta antes del punto donde comienza a haber flicker
 
 // Variables
-int brillo = 0;
-int valDimmer = 0;
+int delaySinceCross = 0;
 String serialInput = "";
 char receivedChar;
 bool newChar;
@@ -23,12 +22,22 @@ void ICACHE_RAM_ATTR zeroCrossInt()
   // Cálculo para 50Hz: 1/50=20mS
   // Entonces medio ciclo= 10mS = 10000uS
   //(10000uS - 10uS) / 128 = 75 (aproximadamente), 10uS propagación
-
-    delayMicroseconds(75 * valDimmer);
+  // (10000uS -10uS) / 1000
+  if (delaySinceCross <= minDelayCross)
+  {
+    digitalWrite(pwmPin, HIGH); // Totalmente prendido
+  }
+  else if (delaySinceCross >= maxDelayCross)
+  {
+    digitalWrite(pwmPin, LOW); // Totalmente apagado
+  }
+  else
+  {
+    delayMicroseconds(delaySinceCross);
     digitalWrite(pwmPin, HIGH);
     delayMicroseconds(10);
     digitalWrite(pwmPin, LOW);
-  // }
+  }
 }
 
 void serialEvent()
@@ -37,17 +46,9 @@ void serialEvent()
   receivedChar = Serial.read();
 }
 
-void setValDimmer(int brillo)
+void setBrillo(int brillo)
 {
-  if (brillo <= minOffset)
-  {
-    brillo = minBrillo;
-  }
-  if (brillo >= maxOffset)
-  {
-    brillo = maxBrillo;
-  } // haciendo esto tengo tiempos muertos para todos los valores en los rangos en los que la luz realmente no varía. Tengo que modificarlo.
-  valDimmer = map(constrain(brillo, minBrillo, maxBrillo), minBrillo, maxBrillo, 128, 0);
+  delaySinceCross = map(brillo, minBrillo, maxBrillo, maxDelayCross, minDelayCross);
 }
 
 void setup()
@@ -58,6 +59,7 @@ void setup()
   pinMode(pwmPin, OUTPUT);
   attachInterrupt(zeroCrossingPin, zeroCrossInt, RISING);
   Serial.onReceive(serialEvent);
+  setBrillo(0);
 }
 // brillo inicial, brillo final, tiempo en segundos
 void fade(int inicial, int final, float fadeTime)
@@ -67,9 +69,8 @@ void fade(int inicial, int final, float fadeTime)
   {
     for (int i = inicial; i <= final; i += 1)
     {
-      brillo = i;
-      Serial.println(brillo);
-      setValDimmer(brillo);
+      Serial.println(i);
+      setBrillo(i);
       delay(stepTime); // fade
     }
   }
@@ -77,47 +78,42 @@ void fade(int inicial, int final, float fadeTime)
   {
     for (int i = inicial; i >= final; i -= 1)
     {
-      brillo = i;
-      Serial.println(brillo);
-      setValDimmer(brillo);
+      Serial.println(i);
+      setBrillo(i);
       delay(stepTime); // fade
     }
-  }
-  else
-  {
-    setValDimmer(inicial); // No debería darse el caso, pero por las dudas :)
   }
 }
 void loop()
 {
-  fade(minOffset, maxOffset, 60);
+  // fade(minBrillo, maxBrillo, 10);
+  // delay(5000);
+  // fade(maxBrillo, minBrillo, 10);
+  // delay(5000);
+  if (newChar)
+  {
+    newChar = false;
+    if (serialInput.length() > 0)
+    {
+      if (receivedChar == '\n') // Veo si es un enter
+      {
+        Serial.print("Brillo: ");
+        Serial.println(serialInput);
+        setBrillo(serialInput.toInt());
+        serialInput = "";
+      }
+      else if (receivedChar == '\b') // Veo si es un backspace
+      {
+        serialInput.remove(serialInput.length() - 1);
+        Serial.println(serialInput);
+      }
+    }
+    else if (isDigit(receivedChar)) // Veo si es un número
+    {
+      // Append the received character to the serialInput string
+      serialInput += receivedChar;
+      Serial.println(serialInput);
+    }
+  }
   delay(500);
-  fade(maxOffset, minOffset, 60);
-  delay(500);
-  // if (newChar)
-  // {
-  //   newChar = false;
-  //   if (receivedChar == '\n') // Veo si es un enter
-  //   {
-  //     brillo = serialInput.toInt();
-  //     Serial.print("Brillo: ");
-  //     Serial.println(brillo);
-  //     setValDimmer(brillo);
-  //     Serial.print("valDimmer: ");
-  //     Serial.println(valDimmer);
-  //     serialInput = "";
-  //   }
-  //   else if (receivedChar == '\b' && serialInput.length() > 0) // Veo si es un backspace
-  //   {
-  //     serialInput.remove(serialInput.length() - 1);
-  //     Serial.println(serialInput);
-  //   }
-  //   else if (isDigit(receivedChar)) // Veo si es un número
-  //   {
-  //     // Append the received character to the serialInput string
-  //     serialInput += receivedChar;
-  //     Serial.println(serialInput);
-  //   }
-  // }
-  // delay(500);
 }
